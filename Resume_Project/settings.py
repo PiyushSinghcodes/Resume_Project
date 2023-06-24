5051483bc04d66dc01ddfd44522e3038a817bf64
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2023-06-25 02:20:58 +0530

    sc

--- Resume_Project/settings.py
+++ Resume_Project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v(#ox_*2=mxxljo6sq37+75sjf7viet1)9u!qh4rzp3fjx63dp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.versel.app']
 
 
 # Application definition
