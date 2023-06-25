c6bce14e204c1416c1ec444b57f2be214eb488ce
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2023-06-25 14:41:09 +0530

    df

--- Resume_Project/wsgi.py
+++ Resume_Project/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Resume_Project.settings')
 
 application = get_wsgi_application()
+app=application
