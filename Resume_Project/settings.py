e156f78967a55a1718edc4bd39675ad668eae905
Piyush Singh <piyushs.ec.21@nitj.ac.in>
2023-06-25 16:11:35 +0530

    d

--- Resume_Project/settings.py
+++ Resume_Project/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'Resume_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+     #   'default': {
+    #        'ENGINE': 'django.db.backends.sqlite3',
+     #       'NAME': BASE_DIR / 'db.sqlite3',
+     #   }
+   # }
 
 
 # Password validation
@@ -122,11 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles_build','static')
 
-MEDIA_URL='img/'
-MEDIA_ROOT=BASE_DIR/'media'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 # Default primary key field type
